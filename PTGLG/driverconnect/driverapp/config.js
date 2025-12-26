330c1bff8d5196354befc14fa33a050ca640dca9
Phisith Tanaphaisansakul <pitpibul.t@gmail.com>
2025-12-26 13:48:01 +0700

    fixup: update test.html to fix layout issues

---
+++ PTGLG/driverconnect/driverapp/config.js
@@ -0,0 +1,8 @@
+// Lightweight client-side config
+// Move environment-specific values here; avoid hardcoding in pages.
+(function(){
+  window.CONFIG = window.CONFIG || {
+    LIFF_ID: '2007705394-y4mV76Gv',
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxn8IxIhL9zdO1QMEyAZ8TThppvApILg7oyGdrPFeKU7L83ClgxxmTKz0bhj0u5ZJM/exec'
+  };
+})();
