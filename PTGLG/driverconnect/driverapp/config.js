47cb0a12add51fd65530e68edcaadf709e8a76e8
Phisith Tanaphaisansakul <pitpibul.t@gmail.com>
2025-12-27 14:02:48 +0700

    Update: Add awareness popup image URL to configuration

--- PTGLG/driverconnect/driverapp/config.js
+++ PTGLG/driverconnect/driverapp/config.js
@@ -3,6 +3,8 @@
 (function(){
   window.CONFIG = window.CONFIG || {
     LIFF_ID: '2007705394-y4mV76Gv',
-    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxn8IxIhL9zdO1QMEyAZ8TThppvApILg7oyGdrPFeKU7L83ClgxxmTKz0bhj0u5ZJM/exec'
+    WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxn8IxIhL9zdO1QMEyAZ8TThppvApILg7oyGdrPFeKU7L83ClgxxmTKz0bhj0u5ZJM/exec',
+    // Awareness popup image URL - แก้ไข URL นี้เป็นลิ้งค์รูปที่ต้องการแสดง
+    AWARENESS_IMAGE_URL: 'https://drive.google.com/file/d/1AODJfyZFFP4WsbrR7kyojPskoQWQcAY1/view?usp=sharing'
   };
 })();
