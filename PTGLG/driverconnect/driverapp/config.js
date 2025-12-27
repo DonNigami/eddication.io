dec3a0772bc73d889201c9fb333e22c51d117040
Phisith Tanaphaisansakul <pitpibul.t@gmail.com>
2025-12-27 14:04:32 +0700

    ok

--- PTGLG/driverconnect/driverapp/config.js
+++ PTGLG/driverconnect/driverapp/config.js
@@ -5,6 +5,4 @@
     LIFF_ID: '2007705394-y4mV76Gv',
     WEB_APP_URL: 'https://script.google.com/macros/s/AKfycbxn8IxIhL9zdO1QMEyAZ8TThppvApILg7oyGdrPFeKU7L83ClgxxmTKz0bhj0u5ZJM/exec',
     // Awareness popup image URL - แก้ไข URL นี้เป็นลิ้งค์รูปที่ต้องการแสดง
-    AWARENESS_IMAGE_URL: 'https://drive.google.com/file/d/1AODJfyZFFP4WsbrR7kyojPskoQWQcAY1/view?usp=sharing'
-  };
-})();
+    AWARENESS_IMAGE_URL: 'https://lh3.googleusercontent. com/d/1AODJfyZFFP4WsbrR7kyojPskoQWQcAY1/
